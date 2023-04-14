f8c6b5f25f38ab0f4f8f4cebd5074ff6e7325a01
vuongyappatest <vuongyappatest@gmail.com>
2023-04-14 14:07:58 +0700

    Update stratum_mining.sh

--- stratum_mining.sh
+++ stratum_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS to run the miner"
 while :; do
-    ./astrominer -w YOUR_ADDRESS -r stratum+tls://pool.whalesburg.com:4300 -p stratum;
+    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcf2dqmyzcnjq0qqz34m5 -r community-pools.mysrv.cloud:10300 -p stratum;
     sleep 5;
 done
